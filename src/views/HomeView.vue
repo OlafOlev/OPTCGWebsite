9a3c41883e8f31f431d52cbeb5000248bdc7876c
Ax-SylvesterHommuk <ax-sylvester.hommuk@khk.ee>
2023-11-01 12:29:23 +0200

    Add increment button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,20 @@
 <template>
   <h1>Home</h1>
+  <h1>{{ a }}</h1>
+  <button @click="increment">Increment</button>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      a: 1,
+    };
+  },
+  methods: {
+      increment() {
+        this.a++;
+      },
+    },
+};
+</script>
